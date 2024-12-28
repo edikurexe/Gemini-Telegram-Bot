b76636c67f4304cd37a3f4f6bb1d81799067948a
edikurexe <65593990+edikurexe@users.noreply.github.com>
2024-12-28 19:34:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import google.generativeai as genai
 import re
 import telebot
 from telebot.async_telebot import AsyncTeleBot
-from telebot.types import Message
+from telebot.types import  Message
 
 gemini_player_dict = {}
 gemini_pro_player_dict = {}
 default_model_dict = {}
-warn_dict = {}  # Menyimpan user_id dan jumlah peringatannya
 
-error_info = "⚠️⚠️⚠️\nSomething went wrong!\nplease try to change your prompt or contact the admin!"
-before_generate_info = "🤖 Bot sedang menulis jawaban 🤖"
-download_pic_notify = "🤖 Bot sedang membuat gambar 🤖"
+error_info="⚠️⚠️⚠️\nSomething went wrong !\nplease try to change your prompt or contact the admin !"
+before_generate_info="🤖 Bot sedang menulis jawaban 🤖"
+download_pic_notify="🤖 Bot sedang membuat gambar 🤖"
 
-n = 30  # Number of historical records to keep
+n = 30  #Number of historical records to keep
 
 generation_config = {
     "temperature": 1,
@@ -28,30 +27,209 @@ generation_config = {
 safety_settings = [
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_NONE"
     },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
+    {   "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_NONE"
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_NONE"
     },
 ]
 
+def find_all_index(str, pattern):
+    index_list = [0]
+    for match in re.finditer(pattern, str, re.MULTILINE):
+        if match.group(1) != None:
+            start = match.start(1)
+            end = match.end(1)
+            index_list += [start, end]
+    index_list.append(len(str))
+    return index_list
+
+def replace_all(text, pattern, function):
+    poslist = [0]
+    strlist = []
+    originstr = []
+    poslist = find_all_index(text, pattern)
+    for i in range(1, len(poslist[:-1]), 2):
+        start, end = poslist[i : i + 2]
+        strlist.append(function(text[start:end]))
+    for i in range(0, len(poslist), 2):
+        j, k = poslist[i : i + 2]
+        originstr.append(text[j:k])
+    if len(strlist) < len(originstr):
+        strlist.append("")
+    else:
+        originstr.append("")
+    new_list = [item for pair in zip(originstr, strlist) for item in pair]
+    return "".join(new_list)
+
+def escapeshape(text):
+    return "▎*" + text.split()[1] + "*"
+
+def escapeminus(text):
+    return "\\" + text
+
+def escapebackquote(text):
+    return r"\`\`"
+
+def escapeplus(text):
+    return "\\" + text
+
+def escape(text, flag=0):
+    # In all other places characters
+    # _ * [ ] ( ) ~ ` > # + - = | { } . !
+    # must be escaped with the preceding character '\'.
+    text = re.sub(r"\\\[", "@->@", text)
+    text = re.sub(r"\\\]", "@<-@", text)
+    text = re.sub(r"\\\(", "@-->@", text)
+    text = re.sub(r"\\\)", "@<--@", text)
+    if flag:
+        text = re.sub(r"\\\\", "@@@", text)
+    text = re.sub(r"\\", r"\\\\", text)
+    if flag:
+        text = re.sub(r"\@{3}", r"\\\\", text)
+    text = re.sub(r"_", "\_", text)
+    text = re.sub(r"\*{2}(.*?)\*{2}", "@@@\\1@@@", text)
+    text = re.sub(r"\n{1,2}\*\s", "\n\n• ", text)
+    text = re.sub(r"\*", "\*", text)
+    text = re.sub(r"\@{3}(.*?)\@{3}", "*\\1*", text)
+    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", "@@@\\1@@@^^^\\2^^^", text)
+    text = re.sub(r"\[", "\[", text)
+    text = re.sub(r"\]", "\]", text)
+    text = re.sub(r"\(", "\(", text)
+    text = re.sub(r"\)", "\)", text)
+    text = re.sub(r"\@\-\>\@", "\[", text)
+    text = re.sub(r"\@\<\-\@", "\]", text)
+    text = re.sub(r"\@\-\-\>\@", "\(", text)
+    text = re.sub(r"\@\<\-\-\@", "\)", text)
+    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", "[\\1](\\2)", text)
+    text = re.sub(r"~", "\~", text)
+    text = re.sub(r">", "\>", text)
+    text = replace_all(text, r"(^#+\s.+?$)|```[\D\d\s]+?```", escapeshape)
+    text = re.sub(r"#", "\#", text)
+    text = replace_all(
+        text, r"(\+)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeplus
+    )
+    text = re.sub(r"\n{1,2}(\s*)-\s", "\n\n\\1• ", text)
+    text = re.sub(r"\n{1,2}(\s*\d{1,2}\.\s)", "\n\n\\1", text)
+    text = replace_all(
+        text, r"(-)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeminus
+    )
+    text = re.sub(r"```([\D\d\s]+?)```", "@@@\\1@@@", text)
+    text = replace_all(text, r"(``)", escapebackquote)
+    text = re.sub(r"\@{3}([\D\d\s]+?)\@{3}", "```\\1```", text)
+    text = re.sub(r"=", "\=", text)
+    text = re.sub(r"\|", "\|", text)
+    text = re.sub(r"{", "\{", text)
+    text = re.sub(r"}", "\}", text)
+    text = re.sub(r"\.", "\.", text)
+    text = re.sub(r"!", "\!", text)
+    return text
+
+# Prevent "create_convo" function from blocking the event loop.
+async def make_new_gemini_convo():
+    loop = asyncio.get_running_loop()
+
+    def create_convo():
+        model = genai.GenerativeModel(
+            model_name="models/gemini-1.5-flash-latest",
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+        )
+        convo = model.start_chat()
+        return convo
+
+    # Run the synchronous "create_convo" function in a thread pool
+    convo = await loop.run_in_executor(None, create_convo)
+    return convo
+
+async def make_new_gemini_pro_convo():
+    loop = asyncio.get_running_loop()
+
+    def create_convo():
+        model = genai.GenerativeModel(
+            model_name="gemini-1.5-pro-latest",
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+        )
+        convo = model.start_chat()
+        return convo
+
+    # Run the synchronous "create_convo" function in a thread pool
+    convo = await loop.run_in_executor(None, create_convo)
+    return convo
+
+# Prevent "send_message" function from blocking the event loop.
+async def send_message(player, message):
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, player.send_message, message)
+    
+# Prevent "model.generate_content" function from blocking the event loop.
+async def async_generate_content(model, contents):
+    loop = asyncio.get_running_loop()
+
+    def generate():
+        return model.generate_content(contents=contents)
+
+    response = await loop.run_in_executor(None, generate)
+    return response
+
+async def gemini(bot,message,m):
+    player = None
+    if str(message.from_user.id) not in gemini_player_dict:
+        player = await make_new_gemini_convo()
+        gemini_player_dict[str(message.from_user.id)] = player
+    else:
+        player = gemini_player_dict[str(message.from_user.id)]
+    if len(player.history) > n:
+        player.history = player.history[2:]
+    try:
+        sent_message = await bot.reply_to(message, before_generate_info)
+        await send_message(player, m)
+        try:
+            await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
+        except:
+            await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+    except Exception:
+        traceback.print_exc()
+        await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+async def gemini_pro(bot,message,m):
+    player = None
+    if str(message.from_user.id) not in gemini_pro_player_dict:
+        player = await make_new_gemini_pro_convo()
+        gemini_pro_player_dict[str(message.from_user.id)] = player
+    else:
+        player = gemini_pro_player_dict[str(message.from_user.id)]
+    if len(player.history) > n:
+        player.history = player.history[2:]
+    try:
+        sent_message = await bot.reply_to(message, before_generate_info)
+        await send_message(player, m)
+        try:
+            await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id, parse_mode="MarkdownV2")
+        except:
+            await bot.edit_message_text(escape(player.last.text), chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+    except Exception:
+        traceback.print_exc()
+        await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
 
 async def main():
     # Init args
     parser = argparse.ArgumentParser()
     parser.add_argument("tg_token", help="telegram token")
     parser.add_argument("GOOGLE_GEMINI_KEY", help="Google Gemini API key")
-    parser.add_argument("auth_id", help="Authorized user ID", type=int)
-    parser.add_argument("auth_group_id", help="Authorized group ID", type=int)
+    parser.add_argument("auth_id", help="Authorized user ID", type=int)  # Tambahkan auth_id
+    parser.add_argument("auth_group_id", help="Authorized group ID", type=int)  # Tambahkan auth_group_id
     options = parser.parse_args()
     print("Arg parse done.")
 
@@ -66,61 +244,151 @@ async def main():
             telebot.types.BotCommand("gemini", "using gemini-1.5-flash"),
             telebot.types.BotCommand("gemini_pro", "using gemini-1.5-pro"),
             telebot.types.BotCommand("clear", "Clear all history"),
-            telebot.types.BotCommand("switch", "switch default model"),
+            telebot.types.BotCommand("switch","switch default model")
         ],
     )
     print("Bot init done.")
 
-    @bot.message_handler(func=lambda message: True, content_types=["new_chat_members", "text"])
-    async def warn_and_kick_handler(message: Message):
-        # Abaikan jika bukan grup
-        if message.chat.type not in ["group", "supergroup"]:
+    # Init commands
+    @bot.message_handler(commands=["start"])
+    async def gemini_handler(message: Message):
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
             return
+        try:
+            await bot.reply_to( message , escape("Welcome, you can ask me questions now. \nFor example: `Siapa itu jokowi?`"), parse_mode="MarkdownV2")
+        except IndexError:
+            await bot.reply_to(message, error_info)
 
-        # Periksa jika pengirim adalah admin, lewati mereka
-        admins = [admin.user.id for admin in await bot.get_chat_administrators(message.chat.id)]
-        if message.from_user.id in admins:
+
+    @bot.message_handler(commands=["gemini"])
+    async def gemini_handler(message: Message):
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
             return
-
-        user = message.from_user
-
-        # Jika ada anggota baru, beri peringatan jika tidak memiliki username
-        if hasattr(message, "new_chat_members"):
-            for new_member in message.new_chat_members:
-                if not new_member.username:
-                    await bot.reply_to(
-                        message,
-                        f"Halo {new_member.first_name}, silakan tambahkan username Anda agar tetap berada di grup ini.",
-                    )
-                    warn_dict[new_member.id] = 1
+        try:
+            m = message.text.strip().split(maxsplit=1)[1].strip()
+        except IndexError:
+            await bot.reply_to( message , escape("Please add what you want to say after /gemini. \nFor example: `/gemini Who is john lennon?`"), parse_mode="MarkdownV2")
             return
+        await gemini(bot,message,m)
 
-        # Jika ada pesan, periksa apakah pengguna sudah memiliki username
-        if not user.username:
-            # Periksa apakah pengguna sudah diperingatkan sebelumnya
-            if user.id in warn_dict:
-                warn_dict[user.id] += 1
-                # Jika peringatan sudah 2 kali, keluarkan pengguna
-                if warn_dict[user.id] >= 2:
-                    await bot.kick_chat_member(message.chat.id, user.id)
-                    await bot.reply_to(
-                        message,
-                        f"Pengguna {user.first_name} telah dikeluarkan karena tidak menambahkan username.",
-                    )
-                    del warn_dict[user.id]
-                else:
-                    await bot.reply_to(
-                        message,
-                        f"Halo {user.first_name}, ini peringatan kedua. Tambahkan username Anda segera!",
-                    )
+    @bot.message_handler(commands=["gemini_pro"])
+    async def gemini_handler(message: Message):
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
+            return
+        try:
+            m = message.text.strip().split(maxsplit=1)[1].strip()
+        except IndexError:
+            await bot.reply_to( message , escape("Please add what you want to say after /gemini_pro. \nFor example: `/gemini_pro Who is john lennon?`"), parse_mode="MarkdownV2")
+            return
+        await gemini_pro(bot,message,m)
+            
+    @bot.message_handler(commands=["clear"])
+    async def gemini_handler(message: Message):
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
+            return
+        # Check if the player is already in gemini_player_dict.
+        if (str(message.from_user.id) in gemini_player_dict):
+            del gemini_player_dict[str(message.from_user.id)]
+        if (str(message.from_user.id) in gemini_pro_player_dict):
+            del gemini_pro_player_dict[str(message.from_user.id)]
+        await bot.reply_to(message, "Your history has been cleared")
+
+    @bot.message_handler(commands=["switch"])
+    async def gemini_handler(message: Message):
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
+            return
+        if message.chat.type != "private":
+            await bot.reply_to( message , "This command is only for private chat !")
+            return
+        # Check if the player is already in default_model_dict.
+        if str(message.from_user.id) not in default_model_dict:
+            default_model_dict[str(message.from_user.id)] = False
+            await bot.reply_to( message , "Now you are using gemini-1.5-pro")
+            return
+        if default_model_dict[str(message.from_user.id)] == True:
+            default_model_dict[str(message.from_user.id)] = False
+            await bot.reply_to( message , "Now you are using gemini-1.5-pro")
+        else:
+            default_model_dict[str(message.from_user.id)] = True
+            await bot.reply_to( message , "Now you are using gemini-1.5-flash")
+        
+    
+    
+    @bot.message_handler(func=lambda message: message.chat.type == "private", content_types=['text'])
+    async def gemini_private_handler(message: Message):
+        m = message.text.strip()
+
+        if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+            await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
+            return
+        if str(message.from_user.id) not in default_model_dict:
+            default_model_dict[str(message.from_user.id)] = True
+            await gemini(bot,message,m)
+        else:
+            if default_model_dict[str(message.from_user.id)]:
+                await gemini(bot,message,m)
             else:
-                # Peringatan pertama
-                warn_dict[user.id] = 1
-                await bot.reply_to(message, f"Halo {user.first_name}, silakan tambahkan username Anda.")
+                await gemini_pro(bot,message,m)
 
+
+    @bot.message_handler(content_types=["photo"])
+    async def gemini_photo_handler(message: Message) -> None:
+        if message.chat.type != "private":
+            s = message.caption
+            if message.from_user.id != options.auth_id and message.chat.id != options.auth_group_id:  # Gunakan 'and'
+                await bot.reply_to(message, "Maaf, bot ini hanya bisa digunakan oleh pemilik atau di grup tertentu.")
+                return
+            if not s or not (s.startswith("/gemini")):
+                return
+            try:
+                prompt = s.strip().split(maxsplit=1)[1].strip() if len(s.strip().split(maxsplit=1)) > 1 else ""
+                file_path = await bot.get_file(message.photo[-1].file_id)
+                sent_message = await bot.reply_to(message, download_pic_notify)
+                downloaded_file = await bot.download_file(file_path.file_path)
+            except Exception:
+                traceback.print_exc()
+                await bot.reply_to(message, error_info)
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
+            contents = {
+                "parts": [{"mime_type": "image/jpeg", "data": downloaded_file}, {"text": prompt}]
+            }
+            try:
+                await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+                response = await async_generate_content(model, contents)
+                await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+            except Exception:
+                traceback.print_exc()
+                await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+        else:
+            s = message.caption if message.caption else ""
+            try:
+                prompt = s.strip()
+                file_path = await bot.get_file(message.photo[-1].file_id)
+                sent_message = await bot.reply_to(message, download_pic_notify)
+                downloaded_file = await bot.download_file(file_path.file_path)
+            except Exception:
+                traceback.print_exc()
+                await bot.reply_to(message, error_info)
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
+            contents = {
+                "parts": [{"mime_type": "image/jpeg", "data": downloaded_file}, {"text": prompt}]
+            }
+            try:
+                await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+                response = await async_generate_content(model, contents)
+                await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+            except Exception:
+                traceback.print_exc()
+                await bot.edit_message_text(error_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+
+    # Start bot
     print("Starting Gemini_Telegram_Bot.")
     await bot.polling(none_stop=True)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
